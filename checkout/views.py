ed24cbb228df1636095cbf626b902ec7e7d7470b
Ahmedaboujeeb <68650816+Ahmedaboujeeb@users.noreply.github.com>
2021-05-11 12:06:58 +0200

    added stripe payments js

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IpsIQIHH9h4Wbq5E3eRjo6XfyXRqLLOj0iYIwqd4K3bUgfpTLPIgEFkhNQCxP5wxTDh8yhh9E3fmyA3XRaQ4Sze00WwH9Doxa',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
